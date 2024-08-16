bf5b6c110dca5f99600844b152f3b836af36e85d
faycarsons <faycarsons23@gmail.com>
2024-08-16 17:24:41 -0400

    add doc comments

--- lib/bytebeat.ml
+++ lib/bytebeat.ml
@@ -1,4 +1,8 @@
-let parse = Parser.parse
-let print_error = Parser.print_error
-let execute = Render.execute
-let run = Render.run
+type parser_error = Error.t
+type program = Ast.t
+
+let parse : string -> (program, parser_error) result = Parser.parse
+let string_of_error : parser_error -> string = Error.pp
+let pp_error : parser_error -> unit = Parser.print_error
+let execute : program -> int -> int = Render.execute
+let run : string -> unit = Render.run
--- lib/bytebeat.mli
+++ lib/bytebeat.mli
@@ -1,4 +1,25 @@
-val parse : string -> (Ast.t, Error.t) result
-val print_error : Error.t -> unit
-val execute : Ast.t -> int -> int
+(** ByteBeat compilation and execution *)
+
+(** A compiled ByteBeat program *)
+type program = Ast.t
+
+type parser_error = Error.t
+
+(** [parse program] Attempts to parse a ByteBeat program, returning either an
+    AST which can be executed with {!execute} or an error which can be printed
+    with {!print_error} *)
+val parse : string -> (program, parser_error) result
+
+val string_of_error : parser_error -> string
+
+(** [print_error error] pretty prints a parser error to stdout for debugging *)
+val pp_error : parser_error -> unit
+
+(** [execute program_ast t]
+    Executes a program for a given t (a monotonic 8khz counter), returning the
+    result *)
+val execute : program -> int -> int
+
+(** [run program]
+    Parses and executes a ByteBeat program on a loop *)
 val run : string -> unit
